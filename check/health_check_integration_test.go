3a83bdbf1971e03ac77ffc1893f68356caf11f1c
Aliaksandr Damaronak <aliaksandr.damaronak@adevinta.com>
2020-10-20 11:24:51 +0300

    fix integration test

--- check/health_check_integration_test.go
+++ check/health_check_integration_test.go
@@ -1,72 +1,73 @@
 package check
 
-// import (
-// 	"sync"
-// 	"testing"
+import (
+	"sync"
+	"testing"
 
-// 	"github.com/golang/mock/gomock"
-// )
+	"github.com/golang/mock/gomock"
+)
 
-// func TestCloseConnectionIsClosing(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
-// 	ck, zk := newZkTestCheck(ctrl)
-// 	stop := make(chan struct{})
-// 	brk := workingBroker(ck, ctrl, stop)
-// 	ck.broker = brk
+func TestCloseConnectionIsClosing(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ck, zk := newZkTestCheck(ctrl)
+	stop := make(chan struct{})
+	brk := workingBroker(ck, ctrl, stop)
+	ck.broker = brk
 
-// 	zk.EXPECT().Connect(gomock.Any(), gomock.Any())
-// 	zk.EXPECT().Get("/brokers/topics/health-check")
-// 	zk.EXPECT().Get("/brokers/topics/replication-check")
-// 	zk.EXPECT().Close()
-// 	brk.EXPECT().DeleteTopic(gomock.Any(), gomock.Any())
+	zk.EXPECT().Connect(gomock.Any(), gomock.Any())
+	zk.EXPECT().Close()
+	brk.EXPECT().DeleteTopic(gomock.Any(), gomock.Any())
 
-// 	zk.EXPECT().Lock(gomock.Any())
-// 	zk.EXPECT().Unlock(gomock.Any())
-// 	brk.EXPECT().Close()
+	zk.EXPECT().Lock(gomock.Any())
+	zk.EXPECT().Unlock(gomock.Any())
+	brk.EXPECT().DeleteTopic(gomock.Any(), gomock.Any())
 
-// 	_ = ck.closeConnection(true)
+	brk.EXPECT().Close()
 
-// 	close(stop)
-// }
+	_ = ck.closeConnection(true)
 
-// func Test_checkHealth_WhenBrokerInMetadataAndProducedMessageIsConsumed_ReportsHealthy(t *testing.T) {
-// 	ctrl := gomock.NewController(t)
-// 	defer ctrl.Finish()
+	close(stop)
+}
 
-// 	stop := make(chan struct{})
-// 	awaitCheck := sync.WaitGroup{}
+func Test_checkHealth_WhenBrokerInMetadataAndProducedMessageIsConsumed_ReportsHealthy(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
 
-// 	check, zk := newZkTestCheck(ctrl)
-// 	connection := workingBroker(check, ctrl, stop)
-// 	connection.EXPECT().Dial(gomock.Any(), gomock.Any()).Return(nil)
-// 	connection.EXPECT().Consumer(gomock.Any()).Return(check.consumer, nil)
-// 	connection.EXPECT().Producer(gomock.Any()).Return(check.producer)
-// 	connection.EXPECT().Metadata().Return(healthyMetadata(check.config.topicName, check.config.replicationTopicName), nil).AnyTimes()
-// 	connection.EXPECT().Close()
-// 	zk.mockHealthyMetadata(check.config.topicName, check.config.replicationTopicName)
+	stop := make(chan struct{})
+	awaitCheck := sync.WaitGroup{}
 
-// 	brokerUpdates := make(chan Update)
-// 	defer close(brokerUpdates)
+	check, zk := newZkTestCheck(ctrl)
+	connection := workingBroker(check, ctrl, stop)
+	connection.EXPECT().Dial(gomock.Any(), gomock.Any()).Return(nil)
+	connection.EXPECT().Consumer(gomock.Any()).Return(check.consumer, nil)
+	connection.EXPECT().Producer(gomock.Any()).Return(check.producer)
+	connection.EXPECT().Metadata().Return(healthyMetadata(check.config.topicName, check.config.replicationTopicName), nil).AnyTimes()
+	connection.EXPECT().Close()
 
-// 	clusterUpdates := make(chan Update)
-// 	defer close(clusterUpdates)
+	zk.mockHealthyMetadata(check.config.topicName, check.config.replicationTopicName)
 
-// 	awaitCheck.Add(1)
-// 	go func() {
-// 		check.CheckHealth(brokerUpdates, clusterUpdates, stop, &awaitCheck)
-// 	}()
+	brokerUpdates := make(chan Update)
+	defer close(brokerUpdates)
 
-// 	brokerStatus := <-brokerUpdates
-// 	clusterStatus := <-clusterUpdates
-// 	close(stop)
-// 	awaitCheck.Wait()
+	clusterUpdates := make(chan Update)
+	defer close(clusterUpdates)
 
-// 	if brokerStatus.Status != insync {
-// 		t.Errorf("CheckHealth reported broker status as %s, expected %s", brokerStatus.Status, insync)
-// 	}
+	awaitCheck.Add(1)
+	go func() {
+		check.CheckHealth(brokerUpdates, clusterUpdates, stop, &awaitCheck)
+	}()
 
-// 	if clusterStatus.Status != green {
-// 		t.Errorf("CheckHealth reported cluster status as %v, expected %s", clusterStatus.Status, green)
-// 	}
-// }
+	brokerStatus := <-brokerUpdates
+	clusterStatus := <-clusterUpdates
+	close(stop)
+	awaitCheck.Wait()
+
+	if brokerStatus.Status != insync {
+		t.Errorf("CheckHealth reported broker status as %s, expected %s", brokerStatus.Status, insync)
+	}
+
+	if clusterStatus.Status != green {
+		t.Errorf("CheckHealth reported cluster status as %v, expected %s", clusterStatus.Status, green)
+	}
+}
